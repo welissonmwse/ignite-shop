1ef37cda391633fbc2288b75ebdea1cc82b48fa6
Welisson Silveira <welissonmwse@gmail.com>
2022-09-10 10:42:03 -0300

    Estrutura visual - Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import logoImg from '../assets/logo.svg'
+
+import * as S from '../styles/pages/app'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <S.Container>
+      <S.Header>
+        <img src={logoImg.src} alt="" />
+      </S.Header>
+      <Component {...pageProps} />
+    </S.Container>
+  )
 }
