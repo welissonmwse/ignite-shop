3be06f2615983ff5cf8972ee3bf0b2bb89e49a56
Welisson Silveira <welissonmwse@gmail.com>
2022-09-10 10:18:17 -0300

    Estrutura visual - Configurando Stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,14 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  theme,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme
+} = createStitches({
+
+})
