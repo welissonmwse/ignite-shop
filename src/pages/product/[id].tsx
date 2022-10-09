ebf9cba9812a1bbfb540bbae136965d0140bcea5
Welisson Silveira <welissonmwse@gmail.com>
2022-10-09 09:04:45 -0300

    Produto & Checkout - Caregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,19 +1,59 @@
+import { GetStaticProps } from 'next'
+import Image from 'next/future/image'
+import Stripe from 'stripe'
+import { stripe } from '../../lib/stripe'
 import * as S from '../../styles/pages/product'
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
+
+export default function Product({product}:ProductProps) {
   return (
     <S.ProductContainer>
       <S.ImageContainer>
-
+        <Image src={product.imageUrl} width={520} height={480} alt=""/>
       </S.ImageContainer>
       <S.ProductDetails>
-        <h1>Camiseta x</h1>
-        <span>R$ 00,00</span>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam a qui magnam iste fugit eius adipisci sed mollitia non doloremque nihil corporis ratione soluta nam ex, corrupti illo officiis unde.</p>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
         <button>
           Comprar agora
         </button>
       </S.ProductDetails>
     </S.ProductContainer>
   )
+}
+
+export const getStaticProps: GetStaticProps<any, {id: string}> = async({params}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: Intl.NumberFormat('pt-br', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hora
+  }
 }
