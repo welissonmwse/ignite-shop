db8c8cb2c2f526fdc8f02bac609b9e247d5abbc3
Welisson Silveira <welissonmwse@gmail.com>
2022-10-09 08:40:34 -0300

    Estrutura visual - Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,19 @@
+import * as S from '../../styles/pages/product'
+
 export default function Product() {
   return (
-    <h1>Product</h1>
+    <S.ProductContainer>
+      <S.ImageContainer>
+
+      </S.ImageContainer>
+      <S.ProductDetails>
+        <h1>Camiseta x</h1>
+        <span>R$ 00,00</span>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam a qui magnam iste fugit eius adipisci sed mollitia non doloremque nihil corporis ratione soluta nam ex, corrupti illo officiis unde.</p>
+        <button>
+          Comprar agora
+        </button>
+      </S.ProductDetails>
+    </S.ProductContainer>
   )
 }
